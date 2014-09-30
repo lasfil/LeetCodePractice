bb19497cb5b0c3644700ac3e6a7998fa7d701ce6
lasfil <lasfil@hotmail.com>
2014-09-30 17:13:36 +0800

    add unique binary search tree

--- src/UniqueBST.java
+++ src/UniqueBST.java
@@ -1,6 +1,36 @@
+import UniqueBST2.TreeNode;
+
+
+/** 
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ *
+ * For example,
+ * 
+ * Given n = 3, there are a total of 5 unique BST's.
+ *
+ *  1         3     3      2      1
+ *   \       /     /      / \      \
+ *    3     2     1      1   3      2
+ *   /     /       \                 \
+ *  2     1         2                 3
+ */
 
 public class UniqueBST {
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 
+		TreeNode(int x) {
+			val = x;
+		}
+
+		TreeNode(int x, TreeNode left, TreeNode right) {
+			val = x;
+			this.left = left;
+			this.right = right;
+		}
+	}
     public int numTrees(int n) {
         if (n <= 1)
             return 1;
