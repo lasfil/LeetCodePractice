20288696c235d9158ef7d7128bcb3875c3d3e458
lasfil <lasfil@hotmail.com>
2014-10-01 12:51:59 +0800

    add validate binary search tree

---
+++ src/ValidateBST.java
@@ -0,0 +1,83 @@
+
+/** 
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ */
+public class ValidateBST {
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+
+		TreeNode(int x, TreeNode left, TreeNode right) {
+			val = x;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public boolean isValidBST(TreeNode root) {
+
+		if (root == null)
+			return true;
+		else if (root.left == null && root.right == null)
+			return true;
+		Boolean result = true;
+		if (root.right != null) {
+			TreeNode rightFirst = findRightFirst(root);
+			if (rightFirst != null && rightFirst.val <= root.val)
+				return false;
+			result &= isValidBST(root.right) & root.right.val > root.val;
+		}
+		if (root.left != null) {
+			TreeNode leftLast = findLeftLast(root);
+			if (leftLast != null && leftLast.val >= root.val)
+				return false;
+			result &= isValidBST(root.left) & root.left.val < root.val;
+		}
+
+		return result;
+	}
+
+	public TreeNode findLeftLast(TreeNode root) {
+		if (root == null || root.left == null)
+			return null;
+
+		TreeNode leftLast = root.left;
+
+		while (leftLast.right != null) {
+			leftLast = leftLast.right;
+		}
+
+		return leftLast;
+	}
+
+	public TreeNode findRightFirst(TreeNode root) {
+		if (root == null || root.right == null)
+			return null;
+
+		TreeNode rightFirst = root.right;
+
+		while (rightFirst.left != null) {
+			rightFirst = rightFirst.left;
+		}
+
+		return rightFirst;
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3, null, new TreeNode(30, new TreeNode(10,
+				null, new TreeNode(15, null, new TreeNode(45))), null));
+		System.out.println(new ValidateBST().isValidBST(root));
+	}
+}
