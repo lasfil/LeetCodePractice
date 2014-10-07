ec8039ef944665f2e54690eb3bfa287b03d92bf0
lasfil <lasfil@hotmail.com>
2014-10-07 19:35:34 +0800

    add containner with most water and search matrix

--- src/SearchMatrix.java
+++ src/SearchMatrix.java
@@ -1,18 +1,14 @@
-/** 
- * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
  *
- * Integers in each row are sorted from left to right.
- * The first integer of each row is greater than the last integer of the previous row.
- * For example,
+ * Integers in each row are sorted from left to right. The first integer of each
+ * row is greater than the last integer of the previous row. For example,
  *
  * Consider the following matrix:
  *
- * [
- *   [1,   3,  5,  7],
- *   [10, 11, 16, 20],
- *   [23, 30, 34, 50]
- * ]
- * Given target = 3, return true.
+ * [ [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50] ] Given target = 3, return
+ * true.
  */
 public class SearchMatrix {
 	public boolean searchMatrix(int[][] matrix, int target) {
@@ -24,21 +20,22 @@ public class SearchMatrix {
 	}
 
 	public boolean searchMatrix(int[][] matrix, int target, int start, int end) {
+		int w = matrix[start].length;
 		if (start == end) {
-			return searchMatrix(matrix[start], target, 0,
-					matrix[start].length - 1);
+			return searchMatrix(matrix[start], target, 0, w - 1);
 		}
 
-		if (start == end)
-			return searchMatrix(matrix[0], target, 0, matrix[0].length);
 		boolean result = false;
 		int mid = (end - start + 1) / 2 + start;
-		if (matrix[mid][0] == target)
+		if (matrix[mid][0] == target || matrix[mid][w - 1] == target)
 			return true;
 		else if (matrix[mid][0] > target)
 			result = searchMatrix(matrix, target, start, mid - 1);
-		else if (matrix[mid][0] < target)
-			result = searchMatrix(matrix, target, mid, end);
+		else if (matrix[mid][w - 1] > target)
+			result = searchMatrix(matrix[mid], target, 0, w-1);
+		else if (matrix[mid][w - 1] < target)
+			result = searchMatrix(matrix, target, mid + 1, end);
+		
 		return result;
 
 	}
@@ -60,9 +57,32 @@ public class SearchMatrix {
 			result = searchMatrix(row, target, mid + 1, end);
 		return result;
 	}
-	
+
+	public boolean searchMatrix1(int[][] matrix, int target) {
+		int length = matrix.length;
+		if (length == 0)
+			return false;
+		int width = matrix[0].length;
+		int low = 0;
+		int high = width * length - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int x = mid / width;
+			int y = mid % width;
+			if (matrix[x][y] == target)
+				return true;
+			else if (matrix[x][y] > target) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
-		int[][] m = new int[][]{{1, 1}};
-		System.out.println(new SearchMatrix().searchMatrix(m, 0));
+		int[][] m = new int[][] { { 1, 2, 3, 4, 5 }, { 10, 20, 30, 40, 50 },
+				{ 100, 200, 300, 400, 500 } };
+		System.out.println(new SearchMatrix().searchMatrix1(m, 0));
 	}
 }
