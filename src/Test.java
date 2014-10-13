64a2374b77c16a5e89f3529b068e9b44225f9cb8
lasfil <lasfil@hotmail.com>
2014-10-13 14:19:40 +0800

    add valid palindrome

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,7 @@
-import java.util.Calendar;
 
 public class Test {
 	public static void main(String[] args) {
-
-		int[] a = new int[] { 2, 2, 2, 3, 3, 3, 4, 4, 4, 0, 0, 1, 2, 2, 2, 2 };
-		System.out.println(Calendar.getInstance().getTimeInMillis());
-		System.out.println(new SearchInRotatedSortedList2().search(a, 0));
-		System.out.println(Calendar.getInstance().getTimeInMillis());
-		System.out.println(new SearchInRotatedSortedList2().search1(a, 0));
-		System.out.println(Calendar.getInstance().getTimeInMillis());
+		String s = "`l;`` 1o1 ??;l`";
+		new ValidPalindrome().isPalindrome(s);
 	}
 }
