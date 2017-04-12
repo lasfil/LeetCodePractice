37ba14c03190b5181292f7bef9eb53171111b046
Liu, Iris <iris.liu@sap.com>
2017-04-12 15:35:09 +0800

    Number Complement

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		char[][] board = new char[][]{{'a','a'}};
-		char[][] board1 = new char[][]{{'a','a'}};
-		System.out.println(new Anagrams().anagrams(new String[]{"abcde","bcdae","abbde", "babde", "abc", "def"}));
-		System.out.println(board.equals(board1));
+		NumberComplement.findComplement(4);
 	}
 }
