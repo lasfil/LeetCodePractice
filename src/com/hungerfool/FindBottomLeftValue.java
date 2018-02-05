96dd0c8effb63a5e501ff945e9ba9754cddc730b
liuhong <iris.liu@sap.com>
2018-02-05 22:56:10 +0800

    no message

--- src/com/hungerfool/FindBottomLeftValue.java
+++ src/com/hungerfool/FindBottomLeftValue.java
@@ -33,8 +33,9 @@ public class FindBottomLeftValue {
 		TreeNode root = new TreeNode(2);
 		root.left = new TreeNode(1);
 		root.right = new TreeNode(3);
-		return findBottomLeftValue(root);
+		return findBottomLeftValue1(root);
 	}
+
 	public static int findBottomLeftValue(TreeNode root) {
 		List<TreeNode> current = new ArrayList<TreeNode>();
 		List<TreeNode> next = new ArrayList<TreeNode>();
@@ -54,5 +55,39 @@ public class FindBottomLeftValue {
 		}
 
 		return current.get(0).val;
+	}
+
+	public static int findBottomLeftValue1(TreeNode root) {
+		int[] temp = findBottomLeftValue(root, 0, root.val);
+		return temp[1];
+	}
+
+	public static int[] findBottomLeftValue(TreeNode root, int height, int val) {
+		int[] temp = new int[2];
+		temp[0] = height;
+		temp[1] = val;
+		if (root != null) {
+			if (root.left == null && root.right == null) {
+				temp[0] = height + 1;
+				temp[1] = root.val;
+			} else {
+				if (root.left != null) {
+					int[] left = findBottomLeftValue(root.left, height + 1, root.left.val);
+					if (left[0] > temp[0]) {
+						temp[0] = left[0];
+						temp[1] = left[1];
+					}
+				}
+				if (root.right != null) {
+					int[] right = findBottomLeftValue(root.right, height + 1, root.right.val);
+					if (right[0] > temp[0]) {
+						temp[0] = right[0];
+						temp[1] = right[1];
+					}
+				}
+			}
+		}
+
+		return temp;
 	}
 }
